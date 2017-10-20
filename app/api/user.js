281f5fad02f83ae4e694318b727d0adf7651bcb6
Renan Lopes <renanlopescoder@gmail.com>
2017-10-20 18:53:40 -0200

    add_users_crud_to_api

--- app/api/user.js
+++ app/api/user.js
@@ -3,6 +3,53 @@ var api = {};
 
 var model = mongoose.model('User');
 
+api.list = function (req, res){
+	model.find({},function(error, list){
+		if(error){
+			res.status(500).json(error);
+		}
+		res.json(list);
+	}); 
+
+};
+
+api.create = function(req, res){
+	model
+		.create(req.body).then(function(dados){
+		res.json(dados);
+	}, function(error){
+		console.log(error);
+		res.status(404).json(error);
+	});
+
+};
+
+api.searchById = function(req,res){
+
+	model
+		.findById(req.params.id)
+		.then(function(id){
+
+			res.json(id);
+
+		}, function(error){
+		console.log(error);
+		res.status(404).json(error);
+	});
+	
+};
+
+api.deleteById = function(req,res){
+	model.remove({_id: req.params.id})
+	.then(function(){
+		res.sendStatus(200);
+	}, function(error){
+		console.log(error);
+		res.status(404).json (error);
+	});
+
+};
+
 api.update = function(req,res){
  console.log(req.body);
 	model
@@ -15,9 +62,7 @@ api.update = function(req,res){
 		console.log(error);
 		res.status(404).json(error);
 	});
+
 };
-
-
-
 
 module.exports = api;
