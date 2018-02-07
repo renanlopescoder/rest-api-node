897420b3fda7797e9b55462685a95812b03c3b86
Renan Lopes <renanlopescoder@gmail.com>
2018-02-07 21:00:04 -0200

    Refactor: huge refactor using ES6 patterns

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -4,41 +4,21 @@ const saltRounds = 15;
 const jwt = require('jsonwebtoken');
 const model = mongoose.model('User');
 
-/**
- * Auth API para gerenciamento de autenticação de usuários
- *
- * Requires: Json Web Token, simple-encryptor, bcrypt e mongoose
- * @description simple-encrytor usado para criptografar dados do usuário em localStorage e Json Web Token para autenticação do usuário no sistema.
- *
- * Modelo: user.js
- * Nome: User
- * Collection no MongoDB: users
- * Atualização: EcmaScript 6
- * Conceitos Aplicados: Criptografia unidirecional e bidirecional Arrow Functions e Variáveis com Escopo de Bloco.
- *
- */
-
 module.exports = (app) => {
-
   let actions = {};
+  const SECRET = app.get('secret');
 
   actions.login = (req, res) => {
     model.findOne({ email: req.body.email })
     .then(
       (user) => {
-        const password = req.body.password;
-        const hash = user.password;
-        bcrypt.compare(password, hash, (error, success) => {
+        bcrypt.compare(req.body.password, user.password, (error, success) => {
           if (error) {
-            console.log('Error, password mismatch with user ' + user.username);
-            res.sendStatus(401);
+            res.status(401).send({ error: error, message: 'Password mismatch'});
           } else if (success) {
-            var token = jwt.sign({ user: user.username }, app.get('secret'),{
-              expiresIn: 84600
-            });
+            const token = jwt.sign({ user_id: user._id }, SECRET, { expiresIn: '3h' });
             return res.status(200).json(
               {
-                user_id: user._id,
                 nickname : user.nickname,
                 username: user.username,
                 photo : user.photo,
@@ -48,35 +28,25 @@ module.exports = (app) => {
             );
           };
         });
-      },
-      (error) => {
-        console.log('Error, user does not exists');
-        res.sendStatus(401);
       }
     )
+    .catch(error => res.status(401).send({ error: error, message: 'Error, user does not exists' }))
   };
 
   actions.verifyToken = (req, res, next) => {
-    let token = req.get('Autorization');
-      if(token) {
-        console.log('Verificando Token');
-        jwt.verify(
-          token,
-          app.get('secret'),
-          (error, decoded) => {
-            if(error) {
-              console.log('Token Rejeitado');
-              res.sendStatus(401);
-            }
-            req.user = decoded;
-            console.log('Usuario aprovado');
-            next();
-          }
-        );
-      } else {
-        console.log('Token não enviado');
-        res.sendStatus(401);
-      }
+    const TOKEN = req.get('Authorization');
+    if (TOKEN) {
+      jwt.verify(TOKEN, SECRET, (error, decoded) => {
+        if (error) {
+          res.status(401).send({ error: error, message: 'Invalid Token' });
+        } else {
+          req.user = decoded;
+          next();
+        }
+      });
+    } else {
+      res.status(401).send('Token is required');
+    };
   };
 
   return actions;
--- src/actions/user.js
+++ src/actions/user.js
@@ -1,19 +1,16 @@
 const mongoose = require('mongoose');
-const mail = require('../services/mail.js');
+const mailService = require('../services/mail.js');
 const bcrypt = require('bcrypt');
 const saltRounds = 15;
-const transporter = mail().getMail();
+const transporter = mailService.config();
 const model = mongoose.model('User');
 
 let actions = {};
 
-actions.list = function (req, res){
-	model.find({},function(error, list){
-		if(error){
-			res.status(500).json(error);
-		}
-		res.json(list);
-	});
+actions.list = (req, res) => {
+	model.find({})
+		.then(users => res.status(200).json(users))
+		.catch(error => res.status(500).json(error))
 };
 
 actions.create = (req,res) => {
@@ -21,72 +18,30 @@ actions.create = (req,res) => {
 		req.body.password = hash;
 		model.create(req.body).then(
 			(user) => {
-				let mailOptions = {
-					from: '"Node API" <rest-api-node@gmail.com>',
-					to: req.body.email,
-					subject: 'Welcome to Node API',
-					html: '<p>Welcome to Node API!</p>'
-						+'<p>This API is developed to provide Front End developers with a server and database configurations for connection testing and to provide a visualization of the data streamlining for the development process. The API provides HTTP methods via AJAX requests to collect, insert and update the given data.</p>'
-						+'Github: <a href="https://github.com/renanlopescoder/rest-api-node">rest-api-node</a>'
-						+'<br><br>Happy Hack!,'
-						+'<br>Node API Team 🤖'
-						+'<br><img src="http://pluspng.com/img-png/github-octocat-logo-vector-png--896.jpg" alt="Github Logo" height="82" width="82">'
-						+'<img src="http://mean.io/wp-content/themes/twentysixteen-child/images/nodejs.png" alt="Node Logo" height="52" width="217">'
-				};
-				transporter.sendMail(mailOptions, (error, info) => {
-						if (error) {
-								return console.log(error);
-						}
-						console.log('Message %s sent: %s', info.messageId, info.response);
-				});
-				res.json(user);
-			},
-			(error) => {
-				console.log(error);
-				res.status(404).json(error);
-		});
-	});
+				const content = mailService.content(req.body.email);
+				transporter.sendMail(content);
+				res.status(200).json(user);
+			})
+			.catch(error => res.status(404).json(error))
+	})
 };
 
-actions.searchById = function(req,res){
-
-	model
-		.findById(req.params.id)
-		.then(function(id){
-
-			res.json(id);
-
-		}, function(error){
-		console.log(error);
-		res.status(404).json(error);
-	});
-
+actions.searchById = (req, res) => {
+	model.findById(req.params.id)
+		.then(user => res.status(200).json(user))
+		.catch(error => res.status(404).json(error))
 };
 
-actions.deleteById = function(req,res){
+actions.deleteById = (req, res) => {
 	model.remove({_id: req.params.id})
-	.then(function(){
-		res.sendStatus(200);
-	}, function(error){
-		console.log(error);
-		res.status(404).json (error);
-	});
-
+		.then(() => res.status(200))
+		.catch(() => res.status(404).json(error))
 };
 
-actions.update = function(req,res){
- console.log(req.body);
-	model
-		.findByIdAndUpdate(req.params.id, req.body)
-		.then(function(dado){
-
-			res.json(dado);
-
-		}, function(error){
-		console.log(error);
-		res.status(404).json(error);
-	});
-
+actions.update = (req, res) => {
+	model.findByIdAndUpdate(req.params.id, req.body)
+		.then(user => res.status(200).json(user))
+		.catch(error => res.status(404).json(error))
 };
 
 module.exports = actions;
