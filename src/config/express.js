279a79126f21968d17d80c61349798481b80081d
Renan Lopes <renanlopes@Renans-MacBook-Pro.local>
2019-09-30 22:07:20 +0200

    Test cors

--- src/config/express.js
+++ src/config/express.js
@@ -4,7 +4,39 @@ const cors = require("cors");
 const consign = require("consign");
 const app = express();
 
-app.use(cors({ origin: "*" }));
+// const allowCrossDomain = (req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, Content-Length, X-Requested-With"
+//   );
+
+//   // intercept OPTIONS method
+//   if ("OPTIONS" == req.method) {
+//     res.send(200);
+//   } else {
+//     next();
+//   }
+// };
+
+// app.all("*", function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
+
+// app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.enable("trust proxy");
 app.use(bodyParser.json());
 app.set("secret", "opensecret");
 
