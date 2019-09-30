4a65c0e8006aca2425746b403927b813b2662111
Renan Lopes <renanlopes@Renans-MacBook-Pro.local>
2019-09-30 22:04:48 +0200

    Cors

--- src/config/express.js
+++ src/config/express.js
@@ -2,34 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const consign = require("consign");
-const server = express();
+const app = express();
 
-const allowCrossDomain = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With"
-  );
-
-  // intercept OPTIONS method
-  if ("OPTIONS" == req.method) {
-    res.send(200);
-  } else {
-    next();
-  }
-};
-
-server.use(allowCrossDomain);
-server.use(bodyParser.json());
-server.set("secret", "opensecret");
-server.use(cors({ origin: "*" }));
+app.use(cors({ origin: "*" }));
+app.use(bodyParser.json());
+app.set("secret", "opensecret");
 
 consign({ cwd: process.cwd() + "/src" })
   .include("app/Models")
   .then("app/Controllers")
   .then("app/Middleware/AuthMiddleware.js")
   .then("routes")
-  .into(server);
+  .into(app);
 
-module.exports = server;
+module.exports = app;
