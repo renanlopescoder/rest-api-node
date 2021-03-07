d3f24cdc5b63723209c130ef50a3c5bb94314071
Renan Lopes <renanlopescoder@gmail.com>
2021-03-07 18:48:57 +0100

    Fix origin error

--- src/config/express.js
+++ src/config/express.js
@@ -9,13 +9,15 @@ const swaggerOptions = require("../../swagger");
 const app = express();
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.ORIGIN || "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json",
+    "Origin, X-Requested, Content-Type, Accept Authorization",
   );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
   next();
 });
 app.use(bodyParser.json());
